59eed6ee6339fc904e7d5bbcdeccf688bf554a40
Joel-Barboza <joelbp5631@gmail.com>
2025-09-07 00:59:13 -0600

    Adding gui and logic files

---
+++ gui.rkt
@@ -0,0 +1,7 @@
+#lang racket/gui
+
+; Make a frame by instantiating the frame% class
+(define frame (new frame% [label "Example"]))
+ 
+; Show the frame by calling its show method
+(send frame show #t)
